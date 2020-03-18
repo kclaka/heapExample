c3c8cff60a5a1949e6d6202974ada78a4e8eef33
kclaka <kennyigbechi@gmail.com>
2020-03-17 17:23:58 -0700

    Add project files.

---
+++ heapExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace heapExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder ref1 = new StringBuilder("object 1");
+            Console.WriteLine(ref1);
+
+            StringBuilder ref2 = new StringBuilder("object 2");
+            Console.WriteLine(ref2);
+            StringBuilder ref3 = ref2;
+
+            Console.WriteLine(ref3);
+        }
+    }
+}
